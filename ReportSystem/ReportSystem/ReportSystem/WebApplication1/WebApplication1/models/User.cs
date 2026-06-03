37338fda525b4e1b0283222843d0a79339615448
TheOkurkac <svarcikradek@seznam.cz>
2026-06-03 11:27:41 +0200

    tgfufzutgtzrfztdfzftzfztf

---
+++ ReportSystem/ReportSystem/ReportSystem/WebApplication1/WebApplication1/models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Nickname { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+    }
+}
+
